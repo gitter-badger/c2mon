d82348206b66d302147cbef13c58435337374405
bcopy <brice.copy@cern.ch>
2019-02-14 16:43:20 +0100

    #158 convert HsqlDB builder to Lombok builder, use properties

--- c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/util/HsqlDatabaseBuilder.java
+++ c2mon-server/c2mon-server-common/src/main/java/cern/c2mon/server/common/util/HsqlDatabaseBuilder.java
@@ -6,55 +6,33 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.Singular;
+
 import javax.sql.DataSource;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
 
-/**
- * @author Justin Lewis Salmon
- */
+@Data
+@Builder
 public class HsqlDatabaseBuilder {
   private String url;
   private String username;
   private String password;
-  private List<Resource> scripts = new ArrayList<>();
-
-  public HsqlDatabaseBuilder url(String url) {
-    this.url = url;
-    return this;
-  }
   
-  public HsqlDatabaseBuilder username(String username) {
-    this.username = username;
-    return this;
-  }
-  
-  public HsqlDatabaseBuilder password(String password) {
-    this.password = password;
-    return this;
-  }
+  @Singular
+  private List<Resource> scripts;
 
-  public HsqlDatabaseBuilder addScript(Resource resource) {
-    this.scripts.add(resource);
-    return this;
-  }
-
-  public DataSource build() {
+  public DataSource toDataSource() {
     DataSource dataSource;
 
-    if (url == null || url.contains("hsqldb:mem")) {
+    if (url == null) {
       // Start an in-process, in-memory HSQL server
       dataSource = new EmbeddedDatabaseBuilder().setType(HSQL).setName("c2mondb").build();
-    } else if (url.contains("hsql://")) {
-      // Start an externally visible, file-based HSQL server
-      HsqlServer.start("file:///tmp/c2mondb", "c2mondb");
-      url += ";sql.syntax_ora=true;hsqldb.default_table_type=cached;hsqldb.cache_rows=1000;hsqldb.result_max_memory_rows=2000;hsqldb.cache_size=100";
-      dataSource = DataSourceBuilder.create().url(url).username(username).password(password).build();
-
     } else {
-      throw new RuntimeException("The given URL was not a valid HSQL JDBC URL!");
+      dataSource = DataSourceBuilder.create().url(url).username(username).password(password).build();
     }
 
     if (!scripts.isEmpty()) {
