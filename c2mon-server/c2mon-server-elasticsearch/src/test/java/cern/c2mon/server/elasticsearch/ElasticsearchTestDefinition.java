ebfccce4bb217fd3818014e33fefbed12f79d479
Alexander Papageorgiou Koufidis <alexander.papageorgiou@cern.ch>
2019-12-18 17:33:34 +0100

    Clean up various cache errors

--- c2mon-server/c2mon-server-elasticsearch/src/test/java/cern/c2mon/server/elasticsearch/ElasticsearchTestDefinition.java
+++ c2mon-server/c2mon-server-elasticsearch/src/test/java/cern/c2mon/server/elasticsearch/ElasticsearchTestDefinition.java
@@ -17,13 +17,14 @@
 package cern.c2mon.server.elasticsearch;
 
 import org.junit.After;
+import cern.c2mon.cache.config.CacheConfigModuleRef;
+
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import cern.c2mon.server.cache.config.CacheModule;
 import cern.c2mon.server.cache.dbaccess.config.CacheDbAccessModule;
-import cern.c2mon.server.cache.loading.config.CacheLoadingModule;
+import cern.c2mon.server.cache.loading.config.CacheLoadingModuleRef;
 import cern.c2mon.server.common.config.CommonModule;
 import cern.c2mon.server.elasticsearch.config.ElasticsearchModule;
 import cern.c2mon.server.elasticsearch.junit.CachePopulationRule;
@@ -32,9 +33,9 @@ import cern.c2mon.server.supervision.config.SupervisionModule;
 
 @ContextConfiguration(classes = {
     CommonModule.class,
-    CacheModule.class,
+    CacheConfigModuleRef.class,
     CacheDbAccessModule.class,
-    CacheLoadingModule.class,
+    CacheLoadingModuleRef.class,
     SupervisionModule.class,
     ElasticsearchModule.class,
     CachePopulationRule.class
